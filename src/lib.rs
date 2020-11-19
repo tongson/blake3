cf307055cb98c6e93918ad32ed154ea2dd9a75af
Eduardo Tongson <propolice@gmail.com>
2020-11-19 21:44:56 +0800

    Indeed it returns raw bytes. Filter through base64 so we get a printeable string.

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,13 @@ use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 extern crate blake3;
+extern crate base64;
 
 #[no_mangle]
 pub extern "C" fn hash(h: *const c_char) -> *const c_char {
     let b = unsafe { CStr::from_ptr(h).to_bytes() };
     let hash = blake3::hash(&b);
-    let c_str = unsafe { CString::from_vec_unchecked(hash.as_bytes().to_vec()) };
+    let c_str = CString::new(base64::encode(hash.as_bytes())).unwrap();
     let ptr = c_str.as_ptr();
     std::mem::forget(c_str);
     return ptr
