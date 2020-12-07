a73189024e0a54a385f31d1350f0613c246b130d
Eduardo Tongson <propolice@gmail.com>
2020-12-07 19:34:35 +0800

    chore: Reindent.

--- src/lib.rs
+++ src/lib.rs
@@ -7,23 +7,23 @@ extern crate base64;
 
 #[no_mangle]
 pub extern "C" fn base64(h: *const c_char) -> *const c_char {
-    panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.base64()")));
-    let b = unsafe { CStr::from_ptr(h).to_bytes() };
-    let hash = blake3::hash(&b);
-    let c_str = CString::new(base64::encode(hash.as_bytes())).unwrap();
-    let ptr = c_str.as_ptr();
-    std::mem::forget(c_str);
-    return ptr
+  panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.base64()")));
+  let b = unsafe { CStr::from_ptr(h).to_bytes() };
+  let hash = blake3::hash(&b);
+  let c_str = CString::new(base64::encode(hash.as_bytes())).unwrap();
+  let ptr = c_str.as_ptr();
+  std::mem::forget(c_str);
+  return ptr
 }
 
 #[no_mangle]
 pub extern "C" fn hash(h: *const c_char) -> *const c_char {
-    panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.hash()")));
-    let b = unsafe { CStr::from_ptr(h).to_bytes() };
-    let hash = blake3::hash(&b);
-    let c_str = CString::new(hash.to_hex().as_str()).unwrap();
-    let ptr = c_str.as_ptr();
-    std::mem::forget(c_str);
-    return ptr
+  panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.hash()")));
+  let b = unsafe { CStr::from_ptr(h).to_bytes() };
+  let hash = blake3::hash(&b);
+  let c_str = CString::new(hash.to_hex().as_str()).unwrap();
+  let ptr = c_str.as_ptr();
+  std::mem::forget(c_str);
+  return ptr
 }
 
