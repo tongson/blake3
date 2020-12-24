b956542b0669600b1ab4be0d1c773237ab1cb416
Eduardo Tongson <propolice@gmail.com>
2020-12-24 12:50:40 +0800

    src: The raw hash has limited utility. The input is expected to be base64 encoded.

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,11 @@ extern crate base64;
 extern crate blake3;
 
 #[no_mangle]
-pub extern "C" fn base64(h: *const c_char) -> *const c_char {
-  panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.base64()")));
-  let b = unsafe { CStr::from_ptr(h).to_bytes() };
-  let hash = blake3::hash(&b);
-  let c_str = CString::new(base64::encode(hash.as_bytes())).unwrap();
-  let ptr = c_str.as_ptr();
-  std::mem::forget(c_str);
-  return ptr;
-}
-
-#[no_mangle]
 pub extern "C" fn hash(h: *const c_char) -> *const c_char {
   panic::set_hook(Box::new(move |_| eprintln!("panic: blake3.hash()")));
   let b = unsafe { CStr::from_ptr(h).to_bytes() };
-  let hash = blake3::hash(&b);
+  let data = base64::decode(&b).unwrap();
+  let hash = blake3::hash(&data);
   let c_str = CString::new(hash.to_hex().as_str()).unwrap();
   let ptr = c_str.as_ptr();
   std::mem::forget(c_str);
